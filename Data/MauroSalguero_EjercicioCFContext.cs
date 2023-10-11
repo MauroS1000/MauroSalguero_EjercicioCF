639f033215d197e368ac31588744f569fcf63e8f
laboratorio <laboratorio@UPOAULA10612>
2023-10-11 08:11:47 -0500

    1er commit

---
+++ Data/MauroSalguero_EjercicioCFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MauroSalguero_EjercicioCF.Models;
+
+namespace MauroSalguero_EjercicioCF.Data
+{
+    public class MauroSalguero_EjercicioCFContext : DbContext
+    {
+        public MauroSalguero_EjercicioCFContext (DbContextOptions<MauroSalguero_EjercicioCFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MauroSalguero_EjercicioCF.Models.Burger> Burger { get; set; } = default!;
+    }
+}
